0648bb05749b2d5939b4d5b824ecedcde11d136e
cwathen23 <cwathen23@gmail.com>
2021-07-14 14:32:35 -0400

    update modal and mobile view

--- js/script.js
+++ js/script.js
@@ -3,10 +3,8 @@
 let openButton = document.querySelector("#menu-trigger");
 let closeButton = document.querySelector("#menu-close");
 let menu = document.querySelector("#side-menu");
-
 openButton.addEventListener("click", toggleMenu);
 closeButton.addEventListener("click", toggleMenu);
-
 function toggleMenu() {
     menu.classList.toggle("show-menu");
 };
@@ -14,36 +12,28 @@ function toggleMenu() {
 //Get More Information Form
 
 var formInput = document.getElementById("more-info-form");
-
 formInput.addEventListener("submit", function(event) {
     event.preventDefault();
-
     let name = document.getElementById("full-name").value;
     name.trim().toLowerCase();
-
     let email = document.getElementById("email").value;
     email.trim().toLowerCase();
-
     let phone = document.getElementById("phone").value;
     phone.trim().toLowerCase();
-
     let message = document.getElementById("message").value;
     message.trim().toLowerCase();
-})
+});
 
 //Smooth Anchor Scrolling
 
 var $root = $('html, body');
-
 $('a[href^="#"]').click(function () {
     var href = $.attr(this, 'href');
-
     $root.animate({
         scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
     }, 1000, function() {
         window.location.hash = href;
     });
-
     return false;
 });
 
@@ -54,21 +44,26 @@ const mouseEvent = e => {
         !e.toElement && 
         !e.relatedTarget &&
         e.clientY < 10;
-
     if (shouldShowExitIntent) {
         document.removeEventListener('mouseout', mouseEvent);
-        
         document.querySelector('.exit-intent-popup').classList.add('visible');
     }
 };
 
-const exit = e => {
-    console.log(e.target.className)
-    const shouldExit =
-        [...e.target.classList].includes('exit-intent-popup') || 
-        e.target.className === 'close' ||
-        e.keyCode === 27;
+let close = document.querySelector(".close");
+close.addEventListener("click", buttonClose);
+function buttonClose (){
+    console.log('clicked');
+    document.querySelector('.exit-intent-popup').classList.remove('visible');
+};
 
+let rootModal = document.querySelector(".modalRoot");
+rootModal.addEventListener("click", buttonClose);
+const exit = e => {
+    console.log(e.target.className);
+    const shouldExit =
+        // e.target.className === 'close' ||
+        e.keyCode === 27;
     if (shouldExit) {
         document.querySelector('.exit-intent-popup').classList.remove('visible');
     }
@@ -78,4 +73,3 @@ setTimeout(() => {
     document.addEventListener('mouseout', mouseEvent);
     document.addEventListener('keydown', exit);
 }, 10_000);
-
